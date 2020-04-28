86882429b09f365e70e89bce81f0abdbd79d2903
日野恵太 <keitahino0415@gmail.com>
2020-04-28 16:58:52 +0900

    環境周り整備

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import MuiThemaProvider from 'material-ui/styles/MuiThemeProvider'
+
 import './index.css';
-import App from './App';
+import reducer from './reducers'
+
 import * as serviceWorker from './serviceWorker';
 
+const enhancer = process.env.NODE_ENV === 'development' ?
+  composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk)
+const store = createStore(reducer, enhancer)
+
 ReactDOM.render(
+  // デバッグモードみたいなもの。本番モード時は影響しない
   <React.StrictMode>
-    <App />
+    {/* material-uiを使用できるようにする */}
+    <MuiThemaProvider>
+      {/* これによりstoreを使えるようになる */}
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/events/new"/>
+            <Route path="/events/:id"/>
+            <Route exact path="/"/>
+            <Route exact path="/events"/>
+          </Switch>
+        </BrowserRouter>
+      </Provider>
+    </MuiThemaProvider>
   </React.StrictMode>,
   document.getElementById('root')
+
 );
 
 // If you want your app to work offline and load faster, you can change
